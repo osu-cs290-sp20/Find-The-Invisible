87f30d0aac4930000d4616e52c35257bc83c5721
BuildTools <unconfigured@null.spigotmc.org>
2020-06-10 03:18:31 -0700

    almost done templatizing

---
+++ public/homePage.js
@@ -0,0 +1,19 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['invisibleThing'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "<button class = "
+    + alias4(((helper = (helper = lookupProperty(helpers,"iconClass") || (depth0 != null ? lookupProperty(depth0,"iconClass") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"iconClass","hash":{},"data":data,"loc":{"start":{"line":1,"column":16},"end":{"line":1,"column":29}}}) : helper)))
+    + ">\r\n  	<img src="
+    + alias4(((helper = (helper = lookupProperty(helpers,"photo") || (depth0 != null ? lookupProperty(depth0,"photo") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"photo","hash":{},"data":data,"loc":{"start":{"line":2,"column":12},"end":{"line":2,"column":21}}}) : helper)))
+    + " alt="
+    + alias4(((helper = (helper = lookupProperty(helpers,"alternate") || (depth0 != null ? lookupProperty(depth0,"alternate") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"alternate","hash":{},"data":data,"loc":{"start":{"line":2,"column":26},"end":{"line":2,"column":39}}}) : helper)))
+    + ">\r\n</button>";
+},"useData":true});
+})();
--- server.js
+++ server.js
@@ -1,10 +1,48 @@
 var express = require('express');
+var exphbs = require('express-handlebars');
+
 var app = express();
 var port = process.env.PORT || 3000;
+var invisibleOptionsContent = require('./invisibleOptions.json');
+app.engine('handlebars', exphbs({defaultLayout: 'theLayout'}));
+app.set('view engine', 'handlebars');
 
-app.use('/', express.static('public'))
+app.use(express.static('public'));
 
+app.get('/', function (req, res) {
+  res.status(200).render('homePage', {
+    invisibleOptions: invisibleOptionsContent,
+    benny: true,
+    hess: false
+    });
+});
+
+app.get('/game/:n', function (req, res, next) {
+  var n = req.params.n;
+  if (n == "benny") {
+    res.status(200).render('gamePage', {
+      photo: "photos/benny.png",
+      benny: true
+    });
+    
+  }
+  else if (n == "hess") {
+    res.status(200).render('gamePage', {
+      photo: "photos/hess.png",
+      hess: true
+    });
+    
+  }
+  else {
+    next();
+  }
+});
+
+app.get('*', function (req, res) {
+  res.status(404).render('404');
+});
 
 app.listen(port, function () {
   console.log("== Server is listening on port", port);
 });
+
